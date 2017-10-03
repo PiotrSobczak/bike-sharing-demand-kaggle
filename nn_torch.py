6031bacd86a0771709503a44b2cdf6536f51fa7c
PiotrSobczak <piosobc@gmail.com>
2017-10-04 01:55:28 +0200

    Added training. Evaluation on train data. Some fixes

--- nn_torch.py
+++ nn_torch.py
@@ -8,6 +8,9 @@ TOTAL_DATASET_SIZE = 10887
 def norm_arr(array):
     return (array - array.min() - (array.max() - array.min()) / 2) / ((array.max() - array.min()) / 2)
 
+def rmsle(y_pred,y_true):
+    return torch.sqrt(torch.mean(torch.pow((torch.log(y_pred + 1) - torch.log(y_true + 1)),2)))
+
 #Reading datasets
 df = pd.read_csv('data/train.csv')
 df_to_predict = pd.read_csv('data/test.csv')
@@ -91,26 +94,33 @@ print("Final train set:",datasetX.head(10))
 train_setX = datasetX
 train_setY = datasetY[['casual','registered','count']].astype(float)
 
+epochs = 50000
+train_data_size = 1000
+layer_dims = {"in": 13, "fc1": 10, "fc2": 10, "fc3": 10,"fc4": 10, "out": 3}
+
 #Conversion from DF to numpyarray
-train_setX = np.array(train_setX)
-train_setY = np.array(train_setY)
+X_train = np.array(train_setX[:train_data_size])
+Y_train = np.array(train_setY[:train_data_size])
 print(train_setX.shape)
-layer_dims = {"in": 12, "fc1": 10, "fc2": 10, "fc3": 10,"fc4": 10, "out": 3}
 
 # Create random Tensors to hold inputs and outputs, and wrap them in Variables.
-X = torch.Tensor(train_setX)
-Y = torch.Tensor(train_setY)
+X_train = Variable(torch.Tensor(X_train))
+Y_train = Variable(torch.Tensor(Y_train))
 #y = Variable(torch.randn(N, D_out), requires_grad=False)
 
 # Use the nn package to define our model and loss function.
 model = torch.nn.Sequential(
-    torch.nn.Tanh(layer_dims["in"], layer_dims["fc1"]),
-    torch.nn.Tanh(layer_dims['fc1'],layer_dims['fc2']),
-    torch.nn.Tanh(layer_dims['fc2'],layer_dims['fc3']),
-    torch.nn.Tanh(layer_dims['fc3'],layer_dims['fc4']),
-    torch.nn.ReLU(layer_dims['fc4'],layer_dims['out'])
+    torch.nn.Linear(layer_dims["in"], layer_dims["fc1"]),
+    torch.nn.Tanh(),
+    torch.nn.Linear(layer_dims['fc1'],layer_dims['fc2']),
+    torch.nn.Tanh(),
+    torch.nn.Linear(layer_dims['fc2'],layer_dims['fc3']),
+    torch.nn.Tanh(),
+    torch.nn.Linear(layer_dims['fc3'],layer_dims['fc4']),
+    torch.nn.Tanh(),
+    torch.nn.Linear(layer_dims['fc4'],layer_dims['out'],
+    torch.nn.ReLU())
 )
-loss_fn = torch.nn.MSELoss(size_average=False)
 
 # Use the optim package to define an Optimizer that will update the weights of
 # the model for us. Here we will use Adam; the optim package contains many other
@@ -118,3 +128,24 @@ loss_fn = torch.nn.MSELoss(size_average=False)
 # optimizer which Variables it should update.
 learning_rate = 1e-4
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for t in range(epochs):
+    # Forward pass: compute predicted y by passing x to the model.
+    y_pred = model(X_train)
+
+    # Compute and print loss.
+    loss = rmsle(y_pred, Y_train)
+    print(t, loss.data[0])
+
+    # Before the backward pass, use the optimizer object to zero all of the
+    # gradients for the variables it will update (which are the learnable weights
+    # of the model)
+    optimizer.zero_grad()
+
+    # Backward pass: compute gradient of the loss with respect to model
+    # parameters
+    loss.backward()
+
+    # Calling the step function on an Optimizer makes an update to its
+    # parameters
+    optimizer.step()
