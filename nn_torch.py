10e5c46f130bad08c6fb1786a6efc2934a4ea4da
PiotrSobczak <piosobc@gmail.com>
2017-10-04 03:11:47 +0200

    Added cross-validation. Todo: Investigate nans, gpu.

--- nn_torch.py
+++ nn_torch.py
@@ -9,7 +9,12 @@ def norm_arr(array):
     return (array - array.min() - (array.max() - array.min()) / 2) / ((array.max() - array.min()) / 2)
 
 def rmsle(y_pred,y_true):
-    return torch.sqrt(torch.mean(torch.pow((torch.log(y_pred + 1) - torch.log(y_true + 1)),2)))
+    log_err = torch.log(y_pred + 1) - torch.log(y_true + 1)
+    squared_le = torch.pow(log_err,2)
+    mean_sle = torch.mean(squared_le)
+    root_msle = torch.sqrt(mean_sle)
+    #print(y_pred.data,y_true.data,log_err.data,squared_le.data,mean_sle.data,root_msle.data)
+    return (root_msle)
 
 #Reading datasets
 df = pd.read_csv('data/train.csv')
@@ -91,22 +96,27 @@ print("Features used:",datasetX.shape)
 print("Final train set:",datasetX.head(1))
 
 #Dividing the original train dataset into train/test set, whole set because keras provides spliting to cross-validation and train set
-#train_setX = datasetX
-#train_setY = datasetY[['casual','registered','count']]
+#ToDo
 
 epochs = 50000
-train_data_size = 1000
+train_data_size = 9600
+batch_size = 64
+steps_in_epoch = train_data_size//batch_size
 layer_dims = {"in": 13, "fc1": 10, "fc2": 10, "fc3": 10,"fc4": 10, "out": 1}
 
 #Conversion from DF to numpyarray
 X_train = np.array(datasetX[:train_data_size])
 Y_train = np.array(datasetY[:train_data_size])
-#print(train_setX.shape)
+X_val = np.array(datasetX[train_data_size:])
+Y_val = np.array(datasetY[train_data_size:])
 
 # Create random Tensors to hold inputs and outputs, and wrap them in Variables.
 X_train = Variable(torch.Tensor(X_train))
 Y_train = Variable(torch.Tensor(Y_train))
-#y = Variable(torch.randn(N, D_out), requires_grad=False)
+X_train_batch = Variable(torch.randn(batch_size, 13))
+Y_train_batch = Variable(torch.randn(batch_size))
+X_val = Variable(torch.Tensor(X_val))
+Y_val = Variable(torch.Tensor(Y_val))
 
 # Use the nn package to define our model and loss function.
 model = torch.nn.Sequential(
@@ -122,30 +132,29 @@ model = torch.nn.Sequential(
     torch.nn.ReLU())
 )
 
-# Use the optim package to define an Optimizer that will update the weights of
-# the model for us. Here we will use Adam; the optim package contains many other
-# optimization algoriths. The first argument to the Adam constructor tells the
-# optimizer which Variables it should update.
-#learning_rate = 1e-4
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(),lr=0.0001)
 
-for t in range(epochs):
-    # Forward pass: compute predicted y by passing x to the model.
-    y_pred = model(X_train)
+mse = torch.nn.MSELoss()
 
-    # Compute and print loss.
-    loss = rmsle(y_pred, Y_train)
-    print(t, loss.data[0])
+for epoch in range(epochs):
+    batch_ind = 0
+    pred_val = model(X_val)
+    val_loss = rmsle(pred_val, Y_val).data[0]
+    pred_train = model(X_train)
+    train_loss = rmsle(pred_train, Y_train).data[0]
+    print("Epoch",epoch,": val loss", val_loss,", train loss:",train_loss)
+    for step in range(steps_in_epoch):
+        # Forward pass: compute predicted y by passing x to the model.
+        X_train_batch = Variable(X_train.data[batch_ind:batch_ind + batch_size])
+        Y_train_batch = Variable(Y_train.data[batch_ind:batch_ind + batch_size])
+        y_pred = model(X_train_batch)
 
-    # Before the backward pass, use the optimizer object to zero all of the
-    # gradients for the variables it will update (which are the learnable weights
-    # of the model)
-    optimizer.zero_grad()
+        # Compute and print loss.
+        loss = mse(y_pred, Y_train_batch)
 
-    # Backward pass: compute gradient of the loss with respect to model
-    # parameters
-    loss.backward()
+        optimizer.zero_grad()
 
-    # Calling the step function on an Optimizer makes an update to its
-    # parameters
-    optimizer.step()
+        loss.backward()
+
+        optimizer.step()
+        batch_ind += batch_size
